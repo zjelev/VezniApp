952a9336d68c133ba08146a5a6b323838c957afa
zjelev <zlatomir@gmail.com>
2023-02-19 18:54:40 +0200

    Add files via upload

---
+++ Models/MeasureViewModel.cs
@@ -0,0 +1,23 @@
+namespace AspNet.Models;
+
+public class MeasureViewModel
+{
+    public int? Kanbel { get; set; }
+    public DateTime Dtime { get; set; }
+    public int? TruckId { get; set; }
+    public string? TruckPlvl { get; set; }
+    public string? Plrem { get; set; }
+    public int? Bruto { get; set; }
+    public DateTime Brutotm { get; set; }
+    public int? Tara { get; set; }
+    public DateTime Taratm { get; set; }
+    public int? Neto { get; set; }
+    public int? VodId { get; set; }
+    public string? Name { get; set; }
+    public string? Sname { get; set; }
+    public string? Fam { get; set; }
+    public int? CompanyId { get; set; }
+    public string? CompanyName { get; set; }
+    public int? Cantnomer { get; set; }
+    public string? ProductName { get; set; }
+}
---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,6 @@
+namespace AspNet.Models;
+
+public class ProductViewModel
+{
+    public string? Name { get; set; }
+}
