d8357c8afa2fa4c1eb828ed092cef53ea392f4a4
zjelev <zlatomir@gmail.com>
2024-04-06 19:28:04 +0300

    remove unnecessary

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,27 +8,10 @@ namespace AspNet.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    public IActionResult Index() => View(MeasuresServices.GetProducts());
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        var products = MeasuresServices.GetProducts();
-        return View(products);
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
